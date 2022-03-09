0e603e861415f1e29ffcc23415e6c2e3e0519258
heymanolito <manuelgallardoffs@gmail.com>
2022-03-09 11:43:33 +0100

    json parsing error

--- cliente.php
+++ cliente.php
@@ -3,14 +3,13 @@
 <body>
 
     <?php
-    // Si se ha hecho una peticion que busca informacion de un autor "get_datos_autor" a traves de su "id"...
     if (isset($_GET["action"]) && isset($_GET["id"])) {
         switch ($_GET["action"]) {
             case "get_datos_autor":
 
-                //Se realiza la peticion a la api que nos devuelve el JSON con la información de los autores
+
                 $app_info = file_get_contents('http://localhost/tarea7rest/api.php?action=get_datos_autor&id=' . $_GET["id"]);
-                // Se decodifica el fichero JSON y se convierte a array
+
                 $app_info = json_decode($app_info, true);
                 //var_dump($app_info);
                 //echo $app_info;
@@ -25,7 +24,7 @@
                 </p>
 
                 <ul>
-                    <!-- Mostramos los libros del autor -->
+
                     <?php foreach ($app_info[1] as $libro) : ?>
                         <li>
                             <?php echo "<a href=\"/tarea7rest/cliente.php?action=get_datos_libros&id={$libro["id"]}\">{$libro["titulo"]}</a>";?>
@@ -33,7 +32,7 @@
                     <?php endforeach; ?>
                 </ul>
                 <br />
-                <!-- Enlace para volver a la lista de autores -->
+
                 <a href="/tarea7rest/cliente.php?action=get_lista_autores" alt="Lista de autores">Volver a la lista de autores</a>
         <?php
                 break;
@@ -67,12 +66,11 @@
         ?>
         <ul>
 
-            <!-- Mostramos una entrada por cada autor -->
             <?php
             //var_dump($lista_autores);
             foreach ($lista_autores as $autores) : ?>
                 <li>
-                    <!-- Enlazamos cada nombre de autor con su informacion (primer if) -->
+
                     <a href="<?php echo "/dwes/tarea7rest/cliente.php?action=get_datos_autor&id=" . $autores["id"]  ?>">
                         <?php echo $autores["nombre"] . " " . $autores["apellidos"] ?>
                     </a>
